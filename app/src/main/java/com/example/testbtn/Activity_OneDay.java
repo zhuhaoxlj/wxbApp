746e70ad1ed4d4d3c3ef62d46c354896c6b6b4e6
MarkGosling <1710589105@qq.com>
2021-12-23 16:48:48 +0800

    android6.0 已经适配，在线版已经完成

--- app/src/main/java/com/example/testbtn/Activity_OneDay.java
+++ app/src/main/java/com/example/testbtn/Activity_OneDay.java
@@ -14,16 +14,15 @@ import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.Map;
-
-import cn.hutool.core.date.DateField;
-import cn.hutool.core.date.DateTime;
-import cn.hutool.core.date.DateUtil;
 
 public class Activity_OneDay extends Activity {
     private TextView enSentence;
@@ -51,12 +50,14 @@ public class Activity_OneDay extends Activity {
             enSentenceAuthor = findViewById(R.id.enSentenceAuthor);
         }
         // 获取今天日期
-        today = DateUtil.today();
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        today = formatter.format(calendar.getTime());
         String m_apiURL = apiURL + today;
         loadImage(m_apiURL);
-
     }
-    public void loadImage(String url){
+
+    public void loadImage(String url) {
         //开启线程加载图片
         new Thread() {
             public void run() {
@@ -65,14 +66,14 @@ public class Activity_OneDay extends Activity {
                 // 获取英文句子、英文句子翻译、英文句子作者、英文图片的网址
                 try {
                     oneDay.init(url);
-/*
+
                     {
                         Log.e("img", oneDay.getEnSentence());
                         Log.e("img", oneDay.getEnSentenceTranslation());
                         Log.e("img", oneDay.getEnSentenceAuthor());
                         Log.e("img", oneDay.getEnImageAndSentence().get(0).toString());
                     }
-*/
+
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -91,25 +92,22 @@ public class Activity_OneDay extends Activity {
                     bitmap = BitmapFactory.decodeStream(imgInputStream);
                     // 获取外部存储目录
                     String sdPath = Environment.getExternalStorageDirectory().getPath();
-                    File file = new File(sdPath + "/AAABBB/21.png");
-                    if (file.exists()) {
-                        FileInputStream fis = new FileInputStream(file);
-                        Bitmap bitmap = BitmapFactory.decodeStream(fis);
-                        Activity activity = (Activity) imageView.getContext();
-                        activity.runOnUiThread(() -> imageView.setImageBitmap(bitmap));
-                    } else {
-                        Activity activity = (Activity) imageView.getContext();
-                        activity.runOnUiThread(new Runnable() {
-
-                            @Override
-                            public void run() {
-                                imageView.setImageBitmap(bitmap);
-                                enSentence.setText(oneDay.getEnSentence());
-                                enSentenceTranslation.setText(oneDay.getEnSentenceTranslation());
-                                enSentenceAuthor.setText(oneDay.getEnSentenceAuthor());
-                            }
-                        });
-                    }
+//                    File file = new File(sdPath + "/AAABBB/22.png");
+//                    if (file.exists()) {
+//                        FileInputStream fis = new FileInputStream(file);
+//                        Bitmap bitmap = BitmapFactory.decodeStream(fis);
+//                        Activity activity = (Activity) imageView.getContext();
+//                        activity.runOnUiThread(() -> imageView.setImageBitmap(bitmap));
+//                    } else {
+//
+//                    }
+                    Activity activity = (Activity) imageView.getContext();
+                    activity.runOnUiThread(() -> {
+                        imageView.setImageBitmap(bitmap);
+                        enSentence.setText(oneDay.getEnSentence());
+                        enSentenceTranslation.setText(oneDay.getEnSentenceTranslation());
+                        enSentenceAuthor.setText(oneDay.getEnSentenceAuthor());
+                    });
                     Log.e("img", "Success");
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -117,34 +115,47 @@ public class Activity_OneDay extends Activity {
             }
         }.start();
     }
+
     /**
      * 按键监听
      */
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_G) {
-            DateTime day = new DateTime(today);
-            DateTime yesterday = day.offset(DateField.DAY_OF_MONTH, -1);
-            String yesterdayStr = yesterday.toDateStr();
-            String m_apiURL = apiURL+yesterdayStr;
+            String yesterdayStr = null;
+            try {
+                yesterdayStr = dateShift("yyyy-MM-dd", today, -1);
+                Log.e("yesterday", yesterdayStr);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            String m_apiURL = apiURL + yesterdayStr;
             today = yesterdayStr;
             loadImage(m_apiURL);
             return true;
         }
 
         if (keyCode == KeyEvent.KEYCODE_H) {
-            DateTime day = new DateTime(today);
-            DateTime tomorrow = day.offset(DateField.DAY_OF_MONTH, 1);
-            String tomorrowStr = tomorrow.toDateStr();
-            String m_apiURL = apiURL+tomorrowStr;
+            // 最多向后33天就没有了
+            String tomorrowStr = null;
+            try {
+                tomorrowStr = dateShift("yyyy-MM-dd", today, 1);
+                Log.e("tomorrow", tomorrowStr);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            String m_apiURL = apiURL + tomorrowStr;
             today = tomorrowStr;
             loadImage(m_apiURL);
             return true;
         }
 
         if (keyCode == KeyEvent.KEYCODE_F) {
-            today = DateUtil.today();
-            String m_apiURL = apiURL+today;
+            Calendar calendar = Calendar.getInstance();
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            today = formatter.format(calendar.getTime());
+            Log.e("today", today);
+            String m_apiURL = apiURL + today;
             loadImage(m_apiURL);
             return true;
         }
@@ -153,5 +164,15 @@ public class Activity_OneDay extends Activity {
 //            return true;
 //        }
         return super.onKeyDown(keyCode, event);
+    }
+
+    public static String dateShift(String dateFormat, String date, int intNum) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat);//格式工具
+        Date da = simpleDateFormat.parse(date);
+        Calendar calendar = new GregorianCalendar();
+        assert da != null;
+        calendar.setTime(da);
+        calendar.add(Calendar.DAY_OF_MONTH, intNum);//日期偏移,正数向前,负数向后!
+        return simpleDateFormat.format(calendar.getTime());
     }
 }
